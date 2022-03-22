efccf656a036098931d3ffb5ec66775b37ef6f8b
olivier3 <olivierbilodeau@outlook.com>
2022-03-22 17:54:33 -0400

    Project base setup

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->middleware(['auth'])->name('index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
