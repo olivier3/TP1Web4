c4b3cc7e89515864e0922b3e9ae152e2144d6d3d
olivier3 <olivierbilodeau@outlook.com>
2022-03-23 22:14:17 -0400

    Add main page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+    <x-slot:header>
+        <h1>RÉPERTOIRE D'IP FRAUDULEUSES</h1>
+    </x-slot:header>
+    <X-main>
+        <div class="flex flex-row-reverse">
+            <form>
+                <input type="text" placeholder="Mettre une IP à rechercher"/>
+                <button type="submit">Rechercher</button>
+            </form>
+        </div>
+    </X-main>
+</x-app-layout>
